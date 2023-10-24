0859076066076d9b693a48201f4986e0206737b2
arar228 <148712960+arar228@users.noreply.github.com>
2023-10-24 23:03:22 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -17,11 +17,23 @@ def talk_to_me(update, context):
     print(text)
     update.message.reply_text(text)
 
+def guess_namber (update, context):
+    print(context.args)
+    if context.args:
+        try:
+            user_number = int(context.args[0 ])
+            message = f"Вы ввели число {user_number}"
+        except  (TypeError, ValueError) :  
+            message = "Введите целое число!!!"
+    else:
+        message = "Введите целое число"
+    update.message.reply_text(message)
+   
 def main():
     mybot = Updater(settings.API_KEY, use_context=True,)
-
     dp = mybot.dispatcher
     dp.add_handler(CommandHandler("start", greet_user))
+    dp.add_handler(CommandHandler("guess", guess_namber))
     dp.add_handler(MessageHandler(Filters.text, talk_to_me))
 
     logging.info("Бот стартовал")
